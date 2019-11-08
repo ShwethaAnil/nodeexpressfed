6c4747dbe5348071bfdd5ef55de45a115ff1c3a1
ShwethaAnil <37101821+ShwethaAnil@users.noreply.github.com>
2019-11-08 13:40:04 +0530

    modules and config

---
+++ app.js
@@ -0,0 +1,113 @@
+const express = require('express');
+var fs = require('fs');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const morgan = require('morgan');
+const config = require('./config/database');
+
+// Article model
+const Article = require('./models/article');
+
+mongoose.connect(config.database);
+const db = mongoose.connection;
+
+// Check connection
+db.once('open', function(){
+  console.log('Connected to MongoDB');
+});
+
+// Check for db errors
+db.on('error', function(err){
+  console.error(err);
+});
+
+const app = express();
+
+// View engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session middleware
+app.use(session({
+  secret: 'some text',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// log all requests to access.log
+app.use(morgan('common', {
+  stream: fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Passport config
+require('./config/passport')(passport);
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('*', function(req, res, next){
+  res.locals.user = req.user || null;
+  next();
+});
+
+app.get('/', function (req, res) {
+  Article.find({}, function(err, articles){
+    if(err){
+      console.error(err);
+    } else {
+      res.render('index', {
+        title: 'Articles', 
+        articles: articles
+      });
+    }
+  });
+});
+
+// Route Files
+let articles = require('./routes/articles');
+let users = require('./routes/users');
+// Any routes that goes to '/articles' will go to the 'articles.js' file in route
+app.use('/articles', articles);
+app.use('/users', users);
+
+app.listen(3200, function(){
+  console.log(`Server started on port 3000`);
+})
